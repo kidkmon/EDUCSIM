e0639967f895e9317df0977e5d819274e2012c7a
Kid Mendes <kidmendesn@gmail.com>
2020-06-24 17:57:11 -0400

    update: add new models

---
+++ Assets/Scripts/LauraCanvas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauraCanvas : MonoBehaviour
+{
+    public void ActiveSkin()
+    {
+        GameObject rafaModel = GameObject.FindGameObjectWithTag("Player");
+        rafaModel.transform.GetChild(0).gameObject.SetActive(true);
+        rafaModel.transform.GetChild(1).gameObject.SetActive(true);
+        rafaModel.transform.GetChild(2).gameObject.SetActive(true);
+    }
+
+    public void ActiveSkull()
+    {
+        GameObject rafaModel = GameObject.FindGameObjectWithTag("Player");
+        rafaModel.transform.GetChild(0).gameObject.SetActive(false);
+        rafaModel.transform.GetChild(1).gameObject.SetActive(true);
+        rafaModel.transform.GetChild(2).gameObject.SetActive(true);
+    }
+
+    public void ActiveBrain()
+    {
+        GameObject rafaModel = GameObject.FindGameObjectWithTag("Player");
+        rafaModel.transform.GetChild(0).gameObject.SetActive(false);
+        rafaModel.transform.GetChild(1).gameObject.SetActive(false);
+        rafaModel.transform.GetChild(2).gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/LauraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauraController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject canvas;
+
+    void Awake()
+    {
+        ActiveSkin();
+        Instantiate(canvas, canvas.transform.position, canvas.transform.rotation);
+    }
+
+    public void ActiveSkin()
+    {
+        GameObject lauraModel = GameObject.FindGameObjectWithTag("Player");
+        lauraModel.transform.GetChild(0).gameObject.SetActive(true);
+        lauraModel.transform.GetChild(1).gameObject.SetActive(true);
+        lauraModel.transform.GetChild(2).gameObject.SetActive(true);
+    }
+}
